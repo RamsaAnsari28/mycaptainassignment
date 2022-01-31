b184c53a325a6f0ff0e41d5dcd57bcb09f5c7f08
Ramsa Ansari <97399728+RamsaAnsari28@users.noreply.github.com>
2022-01-31 18:30:21 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 15 22:58:16 2022
+
+@author: Khalid Ayub Ansari
+"""
+
+org_list =['m','i','s','s','i','s','s','i','p','p','i']
+    
+for i in reversed(org_list):
+    print(i)
+
+    
+    
+    
+    
