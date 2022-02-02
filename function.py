7ffba19af9189f2d09b9e85dfaf8ee1727bd691f
Ramsa Ansari <97399728+RamsaAnsari28@users.noreply.github.com>
2022-02-02 14:19:49 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 15 22:58:16 2022
+
+@author: Khalid Ayub Ansari
+"""
+str ="mississippi"
+
+def make_dict(x):
+    dictionary={}
+    for letter in x:
+        dictionary[letter]=1+ dictionary.get(letter,0)
+    return dictionary
+
+def most_frequent(str):
+    letters=[letter.lower() for letter in str if letter.isalpha()]
+    dictionary=make_dict(letters)
+    result=[]
+    for key in dictionary:
+        result.append((dictionary[key], key))
+    result.sort(reverse=True)
+    for count, letter in result:
+        print(letter, count)
+        
+most_frequent(str)
+    
+
+
+
+    
+    
+    
