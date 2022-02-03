767e590f6a972c31d705d61e57708f0b489556a9
Ramsa Ansari <97399728+RamsaAnsari28@users.noreply.github.com>
2022-02-03 20:51:40 +0530

    Add files via upload

---
+++ e-commerce_website.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 31 19:41:23 2022
+
+@author: Khalid Ayub Ansari
+"""
+
+#e-commerce website:
+ #login page
+ 
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <button type="submit" class="btn btn-primary">login</button>
+</form>
+
+#sign up page
+
+<form>
+  <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputEmail3">
+    </div>
+  </div>
+  <div class="row mb-3">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3">
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary"sign up</button>
+</form>
+
+#product details with descriptions
+
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">college bag</h5>
+    <p class="card-text">college bags for students,childrens anyone can use, stylish for everyday use.</p>
+    <a href="#" class="btn btn-primary">view details</a>
+  </div>
+</div>
